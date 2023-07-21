5b9d93098e4b90410d8ab8ac41ca88a3921a4491
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-22 02:29:04 +0500

    Routing

--- 1-modul/3-dars-Routing/index.js
+++ 1-modul/3-dars-Routing/index.js
@@ -1,9 +1,23 @@
 import express from "express"
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(
+    import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send("Hello I am running")
+    res.sendFile(path.join(__dirname, "views", "index.html"))
+    res.sendStatus(200)
+
+})
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "about.html"))
+})
+app.get('/statusCode', (req, res) => {
+    res.sendStatus(200)
 })
 
 const PORT = process.env.PROT || 4100
