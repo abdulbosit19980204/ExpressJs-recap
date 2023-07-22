9726ab58185644b11de7a1a0806ffe81b0be6c04
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-23 01:04:00 +0500

    Start changed as a call back  function

--- 1-modul/11-dars-MongoDBulashENV/index.js
+++ 1-modul/11-dars-MongoDBulashENV/index.js
@@ -22,15 +22,20 @@ app.use(express.json())
 app.use(authRoutes)
 app.use(productRoutes)
 
+const startApp = () => {
+    try {
 
+        mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        const PORT = process.env.PROT || 4100
+        app.listen(PORT, () => {
+            console.log("Server is running on the PORT =>", PORT);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
-console.log(process.env.MONGO_URI);
-const PORT = process.env.PROT || 4100
-app.listen(PORT, () => {
-    console.log("Server is running on the PORT =>", PORT);
-})
+startApp()
