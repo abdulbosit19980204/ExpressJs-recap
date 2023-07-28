baa4e77232ed8c6d547c41497cab0201bb410e99
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-28 23:00:22 +0500

    Product detail

---
+++ 1-modul/27-dars-ProductDetail/routes/product.js
@@ -0,0 +1,51 @@
+import { Router } from "express";
+import Products from "../models/Product.js";
+import authMiddleware from "../middlewares/auth.js"
+const router = Router()
+
+
+router.get('/', async(req, res) => {
+    const products = await Products.find().lean()
+    res.render('index', {
+        products: products.reverse(),
+        userId: req.userId ? req.userId.toString() : null,
+        title: "APP | Home",
+        isHome: true,
+    })
+})
+
+router.get('/products', async(req, res) => {
+    const user = req.userId ? req.userId.toString() : null
+    const myProducts = await Products.find({ user }).populate('user').lean()
+    res.render('products', {
+        myProducts: myProducts,
+        title: "APP | Products",
+        isProducts: true,
+    })
+})
+router.get('/add', authMiddleware, (req, res) => {
+
+    res.render('add', {
+        title: "APP | Add",
+        isAdd: true,
+        addProductError: req.flash('addProductError')
+    })
+})
+
+// Posts
+
+router.post('/add-products', async(req, res) => {
+    const { title, description, image, price } = req.body
+    if (!title || !description || !image || !price) {
+        req.flash('addProductError', "All fields are required")
+        res.redirect('/add')
+        return
+    }
+    const products = await Products.create({...req.body, user: req.userId })
+
+    res.redirect('/')
+})
+
+
+
+export default router
