a44cc3dec01527663954ebb32e3cda7285882304
abdulbosit19980204 <abdulbosit19980204@gmail.com>
2023-07-30 00:01:49 +0500

    Edit product

--- 1-modul/28-dars-EditProduct/routes/product.js
+++ 1-modul/28-dars-EditProduct/routes/product.js
@@ -33,10 +33,20 @@ router.get('/add', authMiddleware, (req, res) => {
 })
 
 router.get('/product/:id', async(req, res) => {
+    const id = req.params.id
+    const product = await Products.findById(id).populate('user').lean()
+    res.render('product', {
+        product: product
+    })
+})
+
+router.get('/edit-product/:id', async(req, res) => {
         const id = req.params.id
         const product = await Products.findById(id).populate('user').lean()
-        res.render('product', {
-            product: product
+
+        res.render('edit-product', {
+            product: product,
+            errorEditProduct: req.flash('errorEditProduct'),
         })
     })
     // Posts
@@ -53,6 +63,17 @@ router.post('/add-products', async(req, res) => {
     res.redirect('/')
 })
 
+router.post('/edit-product/:id', async(req, res) => {
+    const { title, description, image, price } = req.body
+    const id = req.params.id
+    if (!title || !description || !image || !price) {
+        req.flash('errorEditProduct', "All fields are required")
+        res.redirect('/edit-product')
+        return
+    }
+    const products = await Products.findByIdAndUpdate(id, req.body, { new: true })
 
+    res.redirect('/products')
+})
 
 export default router
